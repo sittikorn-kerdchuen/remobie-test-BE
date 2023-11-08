31db416cc004517d7674bf6416ef4d9bdb9e2cea
unknown <honeytose@gmail.com>
2023-11-09 06:49:07 +0700

    close route

--- controllers/product.js
+++ controllers/product.js
@@ -1,21 +1,21 @@
 const db = require('../config/db'); 
 
-exports.Getdata = async (req, res) => {
-  const q1 = 'SELECT * FROM tb_product as tp ';
-  const q2 = 'INNER JOIN tb_product_sell as tps ';
-  const q3 = 'ON tp.id = tps.brand_name_id ';
-  const q4 = 'JOIN tb_product_name as tpn ';
-  const q5 = 'ON tps.product_name = tpn.id ';
-  const query = q1 + q2 + q3 + q4 + q5;
+// exports.Getdata = async (req, res) => {
+//   const q1 = 'SELECT * FROM tb_product as tp ';
+//   const q2 = 'INNER JOIN tb_product_sell as tps ';
+//   const q3 = 'ON tp.id = tps.brand_name_id ';
+//   const q4 = 'JOIN tb_product_name as tpn ';
+//   const q5 = 'ON tps.product_name = tpn.id ';
+//   const query = q1 + q2 + q3 + q4 + q5;
 
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error('Error retrieving products: ' + err);
-      return res.status(500).json({ error: 'An error occurred' });
-    }
-    res.json(results);
-  });
-};
+//  db.query(query, (err, results) => {
+//     if (err) {
+//       console.error('Error retrieving products: ' + err);
+//       return res.status(500).json({ error: 'An error occurred' });
+//     }
+//     res.json(results);
+//   });
+// };
 
 exports.GetProductName = async (req,res) =>{
   const q1 = 'SELECT * FROM tb_product as tp ';
--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {Getdata,UpdateData,GetProductName} = require('../controllers/product')
 
 // Retrieve tasks
-router.get('/',Getdata)
+// router.get('/',Getdata)
 router.get('/brand',GetProductName)
 router.put('/update/:id',UpdateData)
 
